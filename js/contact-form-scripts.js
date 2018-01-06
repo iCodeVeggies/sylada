e7d15d65f33e95d3679d418128a7b5511b6f14d8
Sylvester Klirowski <sly@Sylvesters-MacBook-Air.local>
2018-01-07 00:19:24 +0100

    initial commit

---
+++ js/contact-form-scripts.js
@@ -0,0 +1,40 @@
+/*
+	AUTHOR: SYLVESTER KLIROWSKI
+*/
+
+$("#contactForm").submit(function (event) {
+    event.preventDefault();
+    //submitForm();
+    $("#contactForm")[0].reset();
+
+    // Thank you toast
+    var $toastContent = $("<span>Thank you for your message. A confirmation has been sent to your email address. (Check the spam folder if you don't see it in your inbox)<span>").add($('<button class="btn-flat toast-action" id="dismiss-toast">X</button>'));
+    Materialize.toast($toastContent, 15000);
+    $(document).on('click', '#dismiss-toast', function() {
+        $(this).parent().remove();
+    });
+    
+});
+
+/* SUBMIT FORM TO SERVER */
+
+function submitForm(){
+// Initiate Variables With Form Content
+var message = $("#message").val();
+var name = $("#name").val();
+var email = $("#email").val();
+var phone = $("#phone").val();
+
+$.ajax({
+    type: "POST",
+    url: "php/form-process.php",
+    data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone
+});
+
+$.ajax({
+    type: "POST",
+    url: "php/contact-conf-email.php",
+    data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone
+});
+
+}
---
+++ php/form-process.php
@@ -0,0 +1,57 @@
+<?php
+/*
+  AUTHOR: SYLVESTER KLIROWSKI
+  GITHUB: https://github.com/voice4eva/
+*/
+$errorMSG = "";
+$fromEMAIL = "info@sylada.com";
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+// PHONE
+$phone = $_POST["phone"];
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+$EmailTo = "klirowski.s@gmail.com";
+$Subject = "Sylada Contact Message";
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $phone;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$fromEMAIL);
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
