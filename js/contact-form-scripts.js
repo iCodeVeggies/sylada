a3ec119a26b6cf128da7c9bd5d424a4e51cc183e
“Sylvester <northeasternhusky@yahoo.com>
2018-09-20 19:46:24 +0200

    changed contact form script to MaterializeV1.x

--- js/contact-form-scripts.js
+++ js/contact-form-scripts.js
@@ -2,40 +2,43 @@
 	AUTHOR: SYLVESTER KLIROWSKI
 */
 
-$("#contactForm").submit(function (event) {
+
+/* MESSAGE SUBMIT */
+var contactForm = document.querySelector('#contactForm');
+contactForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    submitForm();
-    $("#contactForm")[0].reset();
+    contactForm.reset();
 
     // Thank you toast
-    var $toastContent = $("<span>Thank you for your message. A confirmation has been sent to your email address. (Check the spam folder if you don't see it in your inbox)<span>").add($('<button class="btn-flat toast-action" id="dismiss-toast">X</button>'));
-    M.toast($toastContent, 15000);
-    $(document).on('click', '#dismiss-toast', function() {
-        $(this).parent().remove();
-    });
-    
-});
+    M.toast({ html: "<span>Thank you for your message. A confirmation has been sent to your email address. (Check the spam folder if you don't see it in your inbox)<span><button class=\"btn-flat toast-action\">Close</button>", displayLength: 6000 });
+
+    // Close toast if 'close' clicked
+    var toastInstance = M.Toast.getInstance(document.querySelector('.toast'));
+    var toastCloseBtn = document.querySelector('.toast button');
+    toastCloseBtn.addEventListener('click', ()=>{toastInstance.dismiss();})
+})
+
+
 
 /* SUBMIT FORM TO SERVER */
 
-function submitForm(){
-// Initiate Variables With Form Content
-var message = $("#message").val();
-var name = $("#name").val();
-var email = $("#email").val();
-var phone = $("#phone").val();
-console.log(message+" "+name+" "+email+" "+phone);
+function submitForm() {
+    // Initiate Variables With Form Content
+    var message = $("#message").val();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
+    console.log(message + " " + name + " " + email + " " + phone);
 
-$.ajax({
-    type: "POST",
-    url: "php/form-process.php",
-    data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone
-});
+    $.ajax({
+        type: "POST",
+        url: "php/form-process.php",
+        data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone
+    });
 
-$.ajax({
-    type: "POST",
-    url: "php/contact-conf-email.php",
-    data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone
-});
-
+    $.ajax({
+        type: "POST",
+        url: "php/contact-conf-email.php",
+        data: "message=" + message + "&name=" + name + "&email=" + email + "&phone=" + phone
+    });
 }
